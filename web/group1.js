8b3506cf4694f33f164b26cfa57450326deb8f3f
lehitom <48111403+lehitom@users.noreply.github.com>
2020-01-15 18:58:46 -0700

    updated HTML and JS

--- web/group1.js
+++ web/group1.js
@@ -1,3 +1,9 @@
 function buttonClicked() {
 	alert("Clicked!");
 }
+
+function colorChange() {
+	var location = document.getElementById("div1");
+	var color = document.getElementById("bcolor").value;
+	location.style.backgroundColor = color;
+}
