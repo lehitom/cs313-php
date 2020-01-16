47b7dba0807e54bdbe27601b177f94fdef1a9a31
lehitom <48111403+lehitom@users.noreply.github.com>
2020-01-15 18:45:46 -0700

    updated HTML and JS

--- web/group1.js
+++ web/group1.js
@@ -1,16 +1,3 @@
-function clickMe() {
+function buttonClicked() {
 	alert("Clicked!");
-}
-
-function changeColor() {
-	var textbox_id = "txtColor";
-	var textbox = document.getElementById(textbox_id);
-
-	var div_id = "div1";
-	var div = document.getElementById(div_id);
-
-	// We should verify values here before we use them...
-	var color = textbox.value;
-	div.style.backgroundColor = color;
-
 }
