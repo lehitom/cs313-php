e70f5cd13486eae73a41d85a87543dfe0087ec7d
lehitom <48111403+lehitom@users.noreply.github.com>
2020-01-17 22:26:32 -0700

    added homepage.php

---
+++ web/week02home.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Thomas Wood's Home Page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="stylesheet.css" type="text/css">
+  <meta name="description" content="Thomas Wood is an amazing student that looks forward to learning a lot in Web Engineering II.">
+</head>
+
+<body>
+  <header>
+    <div class="header-content">
+      <div class="header-content-main">
+        <h1>Vern's RadioShack</h1>
+        <h2>in Idaho Falls, Idaho</h2>
+        <h3>1755 West Broadway, Idaho Falls, ID 83402</h3>
+      </div>
+    </div>
+  </header>
+  
+  <div class="navbar">
+    <a href="assign08.html">University</a>
+    <a href="assign08_1.html">Academics</a>
+    <a href="assign08_2.html">Athletics</a>
+    <a href="assign08_3.html">Arts</a>
+  </div>
+  
+  <div class="row">
+    
+    <div class="main">
+      <h2>Grand Opening</h2>
+      <h4>Our Grand Opening is in the beginning of April</h4>
+      <h4><b>Our hours are 9 AM to 6 PM, Monday through Saturday.</b></h4><h4>
+      </h4><h5>We are  here and ready to help the community, college student and tinkers alike.</h5>
+      <img src="grand_opening.jpg" alt="Grand Opening">
+    
+      <h2>We Are Open</h2>
+      <h4>We are and have been open since the 6th of March of 2019</h4>
+      <h5>We have plenty of RadioShack stock and a knowledgeable staff. We do repairs for iPhones and help with data recovery for computers.</h5>
+      <img src="inside_store.jpg" alt="Soft Opening">
+    </div>
+	<div class="side">
+      <h2>Our Manager</h2>
+      <h5>Reese Clawson</h5>
+      <img src="Reese.jpg" alt="My face" width="250">
+      <p>Been serving since June of 1982</p>
+      <h3>A brief background</h3>
+      <p><b>Reese</b> was originally a manager for the coporate stores in the Southeastern Idaho Region back in 1982 when he began his relationship with RadioShack.</p><br>
+      <p>He is now the manager of the new Idaho Falls store on Broadway.</p><br>
+      <p>"I'm glad to be up here in my home town, where I can serve my friends and others." -Reese Clawson-</p><br>
+    </div>
+  </div>
+
+  <footer>
+    <div class="footer">
+      <h2>Vern's RadioShack - 208-529-8877 - shackpoky@gmail.com</h2>
+    </div>
+  </footer>
+
+
+</body></html>
