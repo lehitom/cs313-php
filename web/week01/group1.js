cbbe493e6bf9af69acfadf0437eab206d5163e01
lehitom <48111403+lehitom@users.noreply.github.com>
2020-01-25 22:40:57 -0700

    updated directory

---
+++ web/week01/group1.js
@@ -0,0 +1,21 @@
+function buttonClicked() {
+	alert("Clicked!");
+}
+
+function colorChange() {
+	var location = document.getElementById("div1");
+	var color = document.getElementById("bcolor").value;
+	location.style.backgroundColor = color;
+}
+
+$(document).ready(function(){
+	$("#buttonB").click(function(){
+		$("#div1").css("background-color", $("#bcolor").val());
+	});
+});
+
+$(document).ready(function(){
+	$("#buttonA").click(function(){
+		$("#div3").fadeToggle()
+	});
+});
