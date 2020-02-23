fb3f4d3eeb0793023e6e2a224c3236938cd41e8c
lehitom <48111403+lehitom@users.noreply.github.com>
2020-02-22 20:03:20 -0700

    updates

--- web/week07pro/login.php
+++ web/week07pro/login.php
@@ -10,12 +10,25 @@ if(isset($_GET['answer']))
 ?>
 <!DOCTYPE html>
 <html>
-<head lang="en">
-    <meta charset="UTF-8">
-    <title></title>
+<head>
+  <meta charset="utf-8">
+  <title>Login page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="week02home.css" type="text/css">
+  <meta name="description" content="3D note and database tracking">
 </head>
 <body>
-<h1>Login page</h1>
+<header>
+    <div class="header-content">
+      <div class="header-content-main">
+        <h1>Login</h1>
+      </div>
+    </div>
+  </header>
+  
+  <div class="row">
+    
+	<div class="main">
 <form method="post" action="<?php echo htmlspecialchars('verifyuser.php'); ?>">
     User:<br>
     <input type="text" name="user"><br>
@@ -23,6 +36,8 @@ if(isset($_GET['answer']))
     <input type="password" name="password"><br>
     <a href="singup.php">¿Don't have an account? <b>Sign up here!</b></a><br>
     <input type="submit" value="LOGIN">
-</form>
+	</form>
+	</div>
+  </div>
 </body>
 </html>
